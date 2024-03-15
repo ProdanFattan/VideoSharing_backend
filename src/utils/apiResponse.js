8c03df04d01332dc8774933f6be5c1879cca488d
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-03-15 23:04:59 +0600

    api error and responce

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,8 @@
+class apiResponse {
+  constructor(status, data, message = "Success") {
+    this.statusCode = status;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode
+  }
+}
