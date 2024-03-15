56f8a6d0e9ce0db370593a66215065abf244097b
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-03-15 22:09:26 +0600

    server express setting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import express from "express";
-// import { DB_NAME } from "./constants";
+import { app } from "./app.js";
 import connectDB from "./db/connectDB.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
 
 /*
 import mongoose from "mongoose";
